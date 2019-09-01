78d80134f9db9a4c12ba143c341be20cb8bdb129
Domonic Adams <domonicbritton@gmail.com>
2019-09-01 13:43:04 -0700

    Add redirect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Router } from '@reach/router';
 import glamorous from 'glamorous';
 import Home from './containers/home';
@@ -15,34 +15,34 @@ const Wrapper = glamorous.div({
 
 // this is used to use netlify serverless functions inside the same repo
 
-// class LambdaDemo extends Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = { loading: false, msg: null }
-//   }
+class LambdaDemo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { loading: false, msg: null }
+  }
 
-//   handleClick = api => e => {
-//     e.preventDefault()
+  handleClick = api => e => {
+    e.preventDefault()
 
-//     this.setState({ loading: true })
-//     fetch('/.netlify/functions/' + api)
-//       .then(response => response.json())
-//       .then(json => this.setState({ loading: false, msg: json.msg }))
-//   }
+    this.setState({ loading: true })
+    fetch('/.netlify/functions/' + api)
+      .then(response => response.json())
+      .then(json => this.setState({ loading: false, msg: json.msg }))
+  }
 
-//   render() {
-//     const { loading, msg } = this.state
+  render() {
+    const { loading, msg } = this.state
 
-//     return (
-//       <p>
-//         <button onClick={this.handleClick('hello')}>{loading ? 'Loading...' : 'Call Lambda'}</button>
-//         <button onClick={this.handleClick('async-dadjoke')}>{loading ? 'Loading...' : 'Call Async Lambda'}</button>
-//         <br />
-//         <span>{msg}</span>
-//       </p>
-//     )
-//   }
-// }
+    return (
+      <p>
+        <button onClick={this.handleClick('hello')}>{loading ? 'Loading...' : 'Call Lambda'}</button>
+        <button onClick={this.handleClick('async-dadjoke')}>{loading ? 'Loading...' : 'Call Async Lambda'}</button>
+        <br />
+        <span>{msg}</span>
+      </p>
+    )
+  }
+}
 
 const App = () => {
   return (
@@ -51,6 +51,7 @@ const App = () => {
         <Home path="/" />
         <About path="about" />
       </Router>
+      <LambdaDemo />
     </Wrapper>
   );
 };
